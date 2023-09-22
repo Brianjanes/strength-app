ad777165718b001f9758ab9c44b31c149872fa68
Brian Janes <janes.brian@gmail.com>
2023-09-22 16:23:41 -0230

    header stuff

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
-  return <div className="header-main">Header</div>;
+  return (
+    <div className="header-main">
+      <div className="header-title">AI Strength Coach</div>
+      <div className="header-icon-div">
+        <MenuIcon />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
